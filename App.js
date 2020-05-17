c1f2fafa3c0e6b3fb42079281f08045d280d9eab
humichen <43415952+humichen@users.noreply.github.com>
2020-05-18 01:38:26 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { Platform, AsyncStorage } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
 import MainTabNavigator from './src/navigations/MainTabNavigator';
+import { SplashScreen } from 'expo';
 
 import { StoreProvider } from "./src/stores/progressstore";
+const PERSISTENCE_KEY = "ALBUMS_NAVIGATION_STATE2";
 
 const App = () => {
-  return <MainTabNavigator />;
+  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+  const [initialNavigationState, setInitialNavigationState] = React.useState();
+
+  React.useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHide();
+        const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
+        const state = JSON.parse(savedStateString);
+        setInitialNavigationState(state);
+      } catch (e) {
+        // We might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hide();
+      }
+    }
+    loadResourcesAndDataAsync();
+  }, []);
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <NavigationContainer
+        initialState={initialNavigationState}
+        onStateChange={(state) =>
+          AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))
+        }
+      >
+       <MainTabNavigator /> 
+
+      </NavigationContainer>
+    );
+  }
 }
 
 export default () => {
---
+++ MainTabNavigator.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image} from 'react-native';
+
+import{HomeStack,LocationStack,ContactStack,RankStack} from '../screens';
+import LoginScreen from '../screens/LoginScreen';
+import { hide } from 'expo/build/launch/SplashScreen';
+
+const Tab = createBottomTabNavigator();
+const MainTabNavigator = () => {
+    return (
+    // <NavigationContainer>
+      <Tab.Navigator
+      initialRouteName="login"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconPath;
+
+            if (route.name === '首頁') {
+              iconPath = focused
+              ? require('../../assets/ic_home_pressed.png'):
+              require('../../assets/ic_home.png');
+                
+            } else if (route.name === '地點篇') {
+              iconPath = focused
+              ? require('../../assets/ic_location_pressed.png'):
+              require('../../assets/ic_location.png');
+            } else if (route.name == '資訊聯絡篇') {
+              iconPath = focused
+              ? require('../../assets/ic_contact_pressed.png'):
+              require('../../assets/ic_contact.png');
+            } else if (route.name == '排行榜') {
+              iconPath = focused
+              ? require('../../assets/ic_rank_pressed.png'):
+              require('../../assets/ic_rank.png');
+            }
+
+            // You can return any component that you like here!
+            return (
+              <Image 
+                style={{width: 24, height: 24,marginTop:7}}
+                source={iconPath} 
+              />
+            );
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#A7050E',
+          inactiveTintColor: '#707070',
+          labelStyle: {
+            fontSize: 12,
+            marginTop: 5,
+            marginBottom:8,
+            padding: 0,
+          },
+          style:{
+            // height:56,
+            width:"125%",
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: -3 },
+            shadowOpacity: 0.1,
+          },
+        }}
+      >
+        {/* <Tab.Screen name="login" component={LoginScreen}
+        options={() => {
+            return {
+                tabBarVisible: false,
+            };
+          }}/> */}
+        <Tab.Screen name="首頁" component={HomeStack} 
+        options={props => {
+            return {
+                tabBarVisible: !props.route.state || props.route.state.index === 0,
+            };
+        }}
+        />
+        <Tab.Screen name="地點篇" component={LocationStack}  
+        options={props => {
+          return {
+              tabBarVisible: !props.route.state || props.route.state.index === 0,
+          };
+        }}/>
+        <Tab.Screen name="資訊聯絡篇" component={ContactStack}  
+        options={props => {
+          return {
+              tabBarVisible: !props.route.state || props.route.state.index === 0,
+          };
+        }}/>
+        <Tab.Screen name="排行榜" component={RankStack}/>
+        <Tab.Screen name="login" component={LoginScreen}
+        options={() => {
+            return {
+                tabBarVisible: false,
+            };
+          }}/>
+      </Tab.Navigator>
+    // </NavigationContainer>
+    );
+};
+export default MainTabNavigator;
