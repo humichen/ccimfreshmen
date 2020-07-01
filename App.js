f2e69c25bfadac652ec00af7e21f445b9b964342
humichen <43415952+humichen@users.noreply.github.com>
2020-07-01 15:27:57 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
-import React ,{ useContext,useEffect }  from 'react';
+import React, { useContext, useEffect } from 'react';
+import * as firebase from "firebase";
 import { Platform, AsyncStorage } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import MainTabNavigator from './src/navigations/MainTabNavigator';
 import { SplashScreen } from 'expo';
 
-import { StoreProvider,StoreContext } from "./src/stores/progressstore";
+import { StoreProvider, StoreContext } from "./src/stores/progressstore";
 const PERSISTENCE_KEY = "ALBUMS_NAVIGATION_STATE4";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCOGdeW1lZcQT5LitD5EVLBxediP4K4SxE",
+//   authDomain: "ccimfreshmengo.firebaseapp.com",
+//   databaseURL: "https://ccimfreshmengo.firebaseio.com",
+//   projectId: "ccimfreshmengo",
+//   storageBucket: "ccimfreshmengo.appspot.com",
+//   messagingSenderId: "72617957432",
+//   appId: "1:72617957432:web:a2ad710d8ddb14fe7c6ecd"
+// };
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
 
 const App = () => {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
   const [initialNavigationState, setInitialNavigationState] = React.useState();
-  const { meState } = useContext(StoreContext);
+  const { meState, isLoginState } = useContext(StoreContext);
   const [me, setMe] = meState;
+  const [isLogin, setIsLogin] = isLoginState;
 
   React.useEffect(() => {
     async function loadResourcesAndDataAsync() {
@@ -31,40 +46,56 @@ const App = () => {
     loadResourcesAndDataAsync();
   }, []);
 
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCOGdeW1lZcQT5LitD5EVLBxediP4K4SxE",
+      authDomain: "ccimfreshmengo.firebaseapp.com",
+      databaseURL: "https://ccimfreshmengo.firebaseio.com",
+      projectId: "ccimfreshmengo",
+      storageBucket: "ccimfreshmengo.appspot.com",
+      messagingSenderId: "72617957432",
+      appId: "1:72617957432:web:a2ad710d8ddb14fe7c6ecd"
+    };
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+  }, []);
+
   if (!isLoadingComplete) {
     return null;
   } else {
-    if(me.loginstate){
-      return(
-      <NavigationContainer
-        onStateChange={(state) =>
-          AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))
-        }
-      >
-       <MainTabNavigator /> 
-
-      </NavigationContainer>
-      )
-    }else{
+    if (me.loginstate) {
       return (
-      <NavigationContainer
-        initialState={initialNavigationState}
-        onStateChange={(state) =>
-          AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))
-        }
-      >
-       <MainTabNavigator /> 
+        <NavigationContainer
+          onStateChange={(state) =>
+            AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))
+          }
+        >
+          <MainTabNavigator />
 
-      </NavigationContainer>
-    );
+        </NavigationContainer>
+      )
+    } else {
+      return (
+        <NavigationContainer
+          initialState={initialNavigationState}
+          onStateChange={(state) =>
+            AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))
+          }
+        >
+          <MainTabNavigator />
+
+        </NavigationContainer>
+      );
     }
-    
+
   }
 }
 
 export default () => {
   return (
-   <StoreProvider>
-     <App />
-   </StoreProvider>
-  )};
+    <StoreProvider>
+      <App />
+    </StoreProvider>
+  )
+};
